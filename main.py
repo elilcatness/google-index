8e00f0c73bbeba2ad2f8a3b6ddadb32d6152aaad
elilcatness <elilcatness@gmail.com>
2021-12-30 00:25:57 +0300

    refactored a little bit

--- main.py
+++ main.py
@@ -10,28 +10,8 @@ from src.db import db_session
 from src.db.models.state import State
 from src.start import menu
 
-updater: Updater = None
 
-conv_handler = ConversationHandler(
-    entry_points=[CommandHandler('start', menu)],
-    allow_reentry=True,
-    states={
-        'menu': [CallbackQueryHandler(Addition.ask_url, pattern='add_new_domain')],
-        'addition.url': [MessageHandler(Filters.text, Addition.ask_login),
-                         CallbackQueryHandler(menu, pattern='back')],
-        'addition.login': [MessageHandler(Filters.text, Addition.ask_password),
-                           CallbackQueryHandler(Addition.ask_url, pattern='back')],
-        'addition.password': [MessageHandler(Filters.text, Addition.ask_api_key),
-                              CallbackQueryHandler(Addition.ask_login, pattern='back')],
-        'addition.api_key': [MessageHandler(Filters.text, Addition.finish),
-                             CallbackQueryHandler(Addition.ask_password, pattern='back')]
-    },
-    fallbacks=[CommandHandler('start', menu),
-               CallbackQueryHandler(menu, pattern='menu')]
-)
-
-
-def load_states(updater: Updater):
+def load_states(updater: Updater, conv_handler: ConversationHandler):
     with db_session.create_session() as session:
         for state in session.query(State).all():
             conv_handler._conversations[(state.user_id, state.user_id)] = state.callback
@@ -39,11 +19,26 @@ def load_states(updater: Updater):
 
 
 def main():
-    global updater
-
     updater = Updater(os.getenv('token'))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', menu)],
+        allow_reentry=True,
+        states={
+            'menu': [CallbackQueryHandler(Addition.ask_url, pattern='add_new_domain')],
+            'addition.url': [MessageHandler(Filters.text, Addition.ask_login),
+                             CallbackQueryHandler(menu, pattern='back')],
+            'addition.login': [MessageHandler(Filters.text, Addition.ask_password),
+                               CallbackQueryHandler(Addition.ask_url, pattern='back')],
+            'addition.password': [MessageHandler(Filters.text, Addition.ask_api_key),
+                                  CallbackQueryHandler(Addition.ask_login, pattern='back')],
+            'addition.api_key': [MessageHandler(Filters.text, Addition.finish),
+                                 CallbackQueryHandler(Addition.ask_password, pattern='back')]
+        },
+        fallbacks=[CommandHandler('start', menu),
+                   CallbackQueryHandler(menu, pattern='menu')]
+    )
     updater.dispatcher.add_handler(conv_handler)
-    load_states(updater)
+    load_states(updater, conv_handler)
     updater.start_polling()
     updater.idle()
 
